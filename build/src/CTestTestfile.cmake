4fc2556c34e285fd302052ce279b31d43dbe3961
Askhat Khayrullin <clouduckit@gmail.com>
2019-03-29 19:57:05 +0300

    Delete build files

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/toor/tp_game_project/src
-# Build directory: /home/toor/tp_game_project/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
