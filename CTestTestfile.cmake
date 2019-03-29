62e938af0a655ef265e22a7b4545756867deb598
Askhat Khayrullin <clouduckit@gmail.com>
2019-03-29 19:49:44 +0300

    Fixes gitignore

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/toor/tp_game_project
-# Build directory: /home/toor/tp_game_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("build/src")
-subdirs("build/tests")
